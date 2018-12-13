3c9322d8c8f1789670ff9ad792b484d857dd71c7
Alexander Torrijos <artorrijos@gmail.com>
2018-12-13 22:56:03 +0800

    Boostrap navbar toggle working

---
+++ src/app/nav/nav.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+@NgModule({
+  declarations: [MainNavComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MainNavComponent
+  ]
+})
+export class NavModule { }
